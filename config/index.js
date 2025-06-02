df6fc474be733fa5e17d1408d896054373f6c287
shiang <a300251992@gmail.com>
2025-06-02 14:57:24 +0800

    環境建立

--- config/index.js
+++ config/index.js
@@ -1,22 +1,31 @@
-const dotenv = require('dotenv') //載入 dotenv，用來讀取 .env 環境變數檔案
+const dotenv = require("dotenv"); //載入 dotenv，用來讀取 .env 環境變數檔案
 
-const result = dotenv.config()// 執行 dotenv 設定讀取
-// 載入自訂的三個設定檔：資料庫設定、網站設定、機密資料
-const db = require('./db')
-const web = require('./web')
-const secret = require('./secret')
+// Render 上的 Secret File 路徑：/etc/secrets/.env
+const envPath = process.env.RENDER ? "/etc/secrets/.env" : ".env";
 
+const result = dotenv.config({ path: envPath });
 
 // 如果 dotenv 載入失敗，就直接拋出錯誤，讓程式停止
 if (result.error) {
-  throw result.error
+  throw result.error;
+}
+
+// const result = dotenv.config(); // 執行 dotenv 設定讀取
+// // 載入自訂的三個設定檔：資料庫設定、網站設定、機密資料
+const db = require("./db");
+const web = require("./web");
+const secret = require("./secret");
+
+// 如果 dotenv 載入失敗，就直接拋出錯誤，讓程式停止
+if (result.error) {
+  throw result.error;
 }
 // 將所有設定集合成一個物件，供後續使用
 const config = {
   db,
   web,
-  secret
-}
+  secret,
+};
 
 // 建立一個設定管理器，提供統一的方式存取設定值
 class ConfigManager {
@@ -28,25 +37,25 @@ class ConfigManager {
    * @returns {*} - The configuration value corresponding to the given path.
    * @throws Will throw an error if the configuration path is not found.
    */
- // 檢查 path 是否為字串
-  static get (path) {
-    if (!path || typeof path !== 'string') {
-      throw new Error(`incorrect path: ${path}`)
+  // 檢查 path 是否為字串
+  static get(path) {
+    if (!path || typeof path !== "string") {
+      throw new Error(`incorrect path: ${path}`);
     }
-      // 拆解成每層 key，例如 'db.host' => ['db', 'host']
-    const keys = path.split('.')
-    let configValue = config
-     // 遞迴尋找每層設定
+    // 拆解成每層 key，例如 'db.host' => ['db', 'host']
+    const keys = path.split(".");
+    let configValue = config;
+    // 遞迴尋找每層設定
     keys.forEach((key) => {
       if (!Object.prototype.hasOwnProperty.call(configValue, key)) {
-        throw new Error(`config ${path} not found`)
+        throw new Error(`config ${path} not found`);
       }
-      configValue = configValue[key]
-    })
-     // 回傳找到的設定值
-    return configValue
+      configValue = configValue[key];
+    });
+    // 回傳找到的設定值
+    return configValue;
   }
 }
 
 // 匯出 ConfigManager 給其他模組使用
-module.exports = ConfigManager
+module.exports = ConfigManager;
