0398220c510d9b4070c72036b39515a8c89bafa5
shiang <a300251992@gmail.com>
2025-06-02 15:24:53 +0800

    chore: add debug logs to app.js

--- app.js
+++ app.js
@@ -1,51 +1,66 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const pinoHttp = require('pino-http')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const pinoHttp = require("pino-http");
 
-const logger = require('./utils/logger')('App')
-const usersRouter = require('./routes/users')
-const adminRouter = require('./routes/admin')
-const productRouter = require('./routes/products')
-const categoryRouter = require('./routes/category')
-const orderRouter = require('./routes/orders')
+console.log("[Render][app] 基本套件載入完成");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(pinoHttp({
-  logger,
-  serializers: {
-    req (req) {
-      req.body = req.raw.body
-      return req
-    }
-  }
-}))
-app.use(express.static(path.join(__dirname, 'public')))
+const logger = require("./utils/logger")("App");
+const usersRouter = require("./routes/users");
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/products");
+const categoryRouter = require("./routes/category");
+const orderRouter = require("./routes/orders");
 
-app.get('/healthcheck', (req, res) => {
-  res.status(200)
-  res.send('OK')
-})
-app.use('/api/v1/users', usersRouter)
-app.use('/api/v1/admin', adminRouter)
-app.use('/api/v1/products',productRouter)
-app.use('/api/v1/category', categoryRouter)
-app.use('/api/v1/orders',orderRouter)
+console.log("[Render][app] 所有 routes 載入完成");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+console.log("[Render][app] middleware 載入中...");
+
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        req.body = req.raw.body;
+        return req;
+      },
+    },
+  })
+);
+console.log("[Render][app] logger middleware 載入完成");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/healthcheck", (req, res) => {
+  res.status(200);
+  res.send("OK");
+});
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/orders", orderRouter);
+
+console.log("[Render][app] 所有 API route 註冊完成");
 
 app.use((err, req, res, next) => {
-  req.log.error(err)
+  req.log.error(err);
   if (err.status) {
     res.status(err.status).json({
-      message: err.message
-    })
-    return
+      message: err.message,
+    });
+    return;
   }
   res.status(500).json({
-    message: '伺服器錯誤'
-  })
-})
+    message: "伺服器錯誤",
+  });
+});
 
-module.exports = app
+console.log("[Render][app] app.js 結尾");
+
+module.exports = app;
