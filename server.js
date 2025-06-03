8062b6b04b9ff2dfe764b236e1b37b2ce6fe3367
shiang <a300251992@gmail.com>
2025-06-03 14:47:20 +0800

    123

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const app = require("./app");
 
 const { dataSource } = require("./db/data-source");
 
-const PORT = process.env.PORT || 5500;
-
 //初始化資料庫，成功後再啟動伺服器
 
 (async () => {
   try {
     await dataSource.initialize();
-    console.log("[Database] 連線與初始化成功");
+    console.log("[Database] 連線與初始化成功"); // <- 看到這行就代表 initialize 完成
   } catch (err) {
     console.error("[Database] 初始化失敗，錯誤原因：", err);
-    process.exit(1);
+    process.exit(1); // 連線失敗就直接結束程式
   }
 
+  // 如果 initialize 成功，再啟動 Express server
   const PORT = process.env.PORT || 5500;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
